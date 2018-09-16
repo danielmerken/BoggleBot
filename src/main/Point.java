18c7f8170836910caf091f6aca3bf0f7f8706206
Daniel Merken <dcm58@cs.washington.edu>
2018-09-16 15:19:13 -0700

    Javadoc for Point class

--- src/main/Point.java
+++ src/main/Point.java
@@ -1,19 +1,59 @@
 package main;
-
+/**
+ * @author Daniel Merken <dcm58@uw.edu>
+ */
 public class Point {
+	/**
+	 * x-coordinate of this point
+	 */
 	private int x;
+	/**
+	 * y-coordinate of this point
+	 */
 	private int y;
 	
+	/**
+	 * Constructs a new point with the given coordinates
+	 * 
+	 * @param x The x-coordinate of the point to be constructed
+	 * @param y The y-coordinate of the point to be constructed
+	 */
 	public Point(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 	
+	/**
+	 * Returns the x-coordinate of this point
+	 * 
+	 * @return the x-coordinate of this point
+	 */
 	public int getX() {
 		return x;
 	}
 	
+	/**
+	 * Returns the y-coordinate of this point
+	 * 
+	 * @return the y-coordinate of this point
+	 */
 	public int getY() {
 		return y;
 	}
+	
+	 @Override
+	 public int hashCode() {
+		 int hashCode = x;
+		 hashCode += 31 * y;
+		 return hashCode;
+	 }
+	 
+	 @Override
+	 public boolean equals(Object o) {
+		 if (!(o instanceof Point)) {
+			 return false;
+		 }
+		 Point p = (Point) o;
+		 return p.x == this.x && p.y == this.y;
+	 }
 }
