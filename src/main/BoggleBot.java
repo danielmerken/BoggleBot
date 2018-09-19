9810f631bcbcbedb16e93a6e85e4cc73bd2c9c9e
Daniel Merken <dcm58@cs.washington.edu>
2018-09-19 16:54:10 -0700

    Finished BoggleBot javadoc and added a linear solution

--- src/main/BoggleBot.java
+++ src/main/BoggleBot.java
@@ -1,10 +1,11 @@
 package main;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
 /**
- * Class that contains a parallelized method to solve a board. 
+ * Class that contains a parallelized and linear method to solve a board. 
  * 
  * @author Daniel Merken <dcm58@uw.edu>
  */
@@ -14,6 +15,7 @@ public class BoggleBot {
 	/**
 	 * Finds all words in the given dictionary that are contained in the
 	 * given boggle board and stores results in the supplied BoggleBoard
+	 * in parallel
 	 * 
 	 * @param dictionary Contains a dictionary of which words to search for
 	 * @param board The board in which to search for words
@@ -22,9 +24,43 @@ public class BoggleBot {
 		for (int i = 0; i < board.getWidth(); i++) {
 			for (int j = 0; j < board.getHeight(); j++) {
 				Point p = new Point(i, j);
-				if (dictionary.containsPrefix("" + board.get(p))) {
+				if (dictionary.containsPrefix(board.get(p))) {
 					BogglePath path = new BogglePath(board, p);
 					POOL.invoke(new BoggleBotTask(dictionary, board, path));
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Finds all words in the given dictionary that are contained in the
+	 * given boggle board and stores results in the supplied BoggleBoard
+	 * in linear
+	 * 
+	 * @param dictionary Contains a dictionary of which words to search for
+	 * @param board The board in which to search for words
+	 */
+	public static void solveBoardLinear(Trie dictionary, BoggleBoard board) {
+		for (int i = 0; i < board.getWidth(); i++) {
+			for (int j = 0; j < board.getHeight(); j++) {
+				if (dictionary.containsPrefix(board.get(i, j))) {
+					Stack<BogglePath> dfs = new Stack<BogglePath>();
+					dfs.push(new BogglePath(board, new Point(i, j)));
+					while (!dfs.isEmpty()) {
+						BogglePath currPath = dfs.pop();
+						if (dictionary.containsWord(currPath.getWord())) {
+							board.addSolution(currPath);
+						}
+						for (Point p : board.getAdjPoints(currPath.getLastPoint())) {
+							if (!currPath.containsPoint(p) && 
+									dictionary.containsPrefix(
+											currPath.getWord() + board.get(p))) {
+								BogglePath nextPath = new BogglePath(currPath);
+								nextPath.addPoint(p);
+								dfs.push(nextPath);
+							}
+						}
+					}
 				}
 			}
 		}
