fa97312bc6bdbcda0675980926eb61431c72510b
Daniel Merken <dcm58@cs.washington.edu>
2018-09-19 17:01:31 -0700

    Added method that only parses the dictionary resources folder

--- src/main/DictionaryParser.java
+++ src/main/DictionaryParser.java
@@ -2,6 +2,7 @@ package main;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 /**
  * Class that contains a method that fills and returns a trie with the contents
  * of a specified file. A minimum length can be specified, meaning lines of 
@@ -23,11 +24,51 @@ public class DictionaryParser {
 	 * @return A trie containing every line of text in the specified file that
 	 *         is of at least the specified length
 	 */
-	public static Trie parseDictionary(String filename, int minLength) {
+	public static Trie parseFile(String filename, int minLength) {
 		BufferedReader reader = null;
 		Trie result = new Trie();
 		try {
 			reader = new BufferedReader(new FileReader(filename));
+			// Add one word at a time
+			String inputLine;
+			while ((inputLine = reader.readLine()) != null) {
+				if (inputLine.length() >= minLength) {
+					result.insert(inputLine);
+				}
+			}
+		} catch (IOException e) {
+			System.err.println(e.toString());
+			e.printStackTrace(System.err);
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					System.err.println(e.toString());
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Reads and stores every word in the dictionary into a trie, then returns
+	 * said trie. Ignores words in the dictionary that are shorter than the 
+	 * specified minimum length.
+	 * 
+	 * @param minLength Minimum length of words to be returned, shorter words 
+	 *        will be ignored
+	 * @return A trie containing every word in the dictionary that
+	 *         is of at least the specified length
+	 */
+	public static Trie parseDictionary(int minLength) {
+		BufferedReader reader = null;
+		Trie result = new Trie();
+		try {
+			reader = new BufferedReader(new InputStreamReader(
+					DictionaryParser.class.getResourceAsStream(
+							"/main/resources/dictionary.txt")));
 			// Add one word at a time
 			String inputLine;
 			while ((inputLine = reader.readLine()) != null) {
