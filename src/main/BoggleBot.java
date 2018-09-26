01658bdc62e1b91202a710a4ff725a79c7e455e2
Daniel Merken <dcm58@cs.washington.edu>
2018-09-26 13:35:19 -0700

    Redid parallel solution

--- src/main/BoggleBot.java
+++ src/main/BoggleBot.java
@@ -11,6 +11,7 @@ import java.util.concurrent.RecursiveAction;
  */
 public class BoggleBot {
 	private static final ForkJoinPool POOL = new ForkJoinPool();
+	private static final int SEQUENTIAL_CUTOFF = 8;
 	
 	/**
 	 * Finds all words in the given dictionary that are contained in the
@@ -21,15 +22,7 @@ public class BoggleBot {
 	 * @param board The board in which to search for words
 	 */
 	public static void solveBoard(Trie dictionary, BoggleBoard board) {
-		for (int i = 0; i < board.getWidth(); i++) {
-			for (int j = 0; j < board.getHeight(); j++) {
-				Point p = new Point(i, j);
-				if (dictionary.containsPrefix(board.get(p))) {
-					BogglePath path = new BogglePath(board, p);
-					POOL.invoke(new BoggleBotTask(dictionary, board, path));
-				}
-			}
-		}
+		POOL.invoke(new BoggleBotTask(dictionary, board, 0, board.getWidth() * board.getHeight()));
 	}
 	
 	/**
@@ -43,23 +36,27 @@ public class BoggleBot {
 	public static void solveBoardLinear(Trie dictionary, BoggleBoard board) {
 		for (int i = 0; i < board.getWidth(); i++) {
 			for (int j = 0; j < board.getHeight(); j++) {
-				if (dictionary.containsPrefix(board.get(i, j))) {
-					Stack<BogglePath> dfs = new Stack<BogglePath>();
-					dfs.push(new BogglePath(board, new Point(i, j)));
-					while (!dfs.isEmpty()) {
-						BogglePath currPath = dfs.pop();
-						if (dictionary.containsWord(currPath.getWord())) {
-							board.addSolution(currPath);
-						}
-						for (Point p : board.getAdjPoints(currPath.getLastPoint())) {
-							if (!currPath.containsPoint(p) && 
-									dictionary.containsPrefix(
-											currPath.getWord() + board.get(p))) {
-								BogglePath nextPath = new BogglePath(currPath);
-								nextPath.addPoint(p);
-								dfs.push(nextPath);
-							}
-						}
+				solveBoardFromPoint(dictionary, board, new Point(i, j));
+			}
+		}
+	}
+	
+	public static void solveBoardFromPoint(Trie dictionary, BoggleBoard board, Point start) {
+		if (dictionary.containsPrefix(board.get(start))) {
+			Stack<BogglePath> dfs = new Stack<BogglePath>();
+			dfs.push(new BogglePath(board, start));
+			while (!dfs.isEmpty()) {
+				BogglePath currPath = dfs.pop();
+				if (dictionary.containsWord(currPath.getWord())) {
+					board.addSolution(currPath);
+				}
+				for (Point p : board.getAdjPoints(currPath.getLastPoint())) {
+					if (!currPath.containsPoint(p) && 
+							dictionary.containsPrefix(
+									currPath.getWord() + board.get(p))) {
+						BogglePath nextPath = new BogglePath(currPath);
+						nextPath.addPoint(p);
+						dfs.push(nextPath);
 					}
 				}
 			}
@@ -70,36 +67,28 @@ public class BoggleBot {
 		
 		private Trie dictionary;
 		private BoggleBoard board;
-		private BogglePath currPath;
+		private int lo;
+		private int hi;
 		
-		public BoggleBotTask(Trie dictionary, BoggleBoard board, 
-				BogglePath currPath) {
+		public BoggleBotTask(Trie dictionary, BoggleBoard board, int lo, int hi) {
 			this.dictionary = dictionary;
 			this.board = board;
-			this.currPath = currPath;
+			this.lo = lo;
+			this.hi = hi;
 		}
 		
 		@Override
 		protected void compute() {
-			if (dictionary.containsWord(currPath.getWord())) {
-				board.addSolution(currPath);
-			}
-			List<Point> adjPoints = board.getAdjPoints(currPath.getLastPoint());
-			List<BoggleBotTask> threads = new ArrayList<BoggleBotTask>();
-			for (Point p : adjPoints) {
-				if (!currPath.containsPoint(p) && 
-						dictionary.containsPrefix(
-								currPath.getWord() + board.get(p))) {
-					BogglePath nextPath = new BogglePath(currPath);
-					nextPath.addPoint(p);
-					threads.add(new BoggleBotTask(dictionary, board, nextPath));
+			if (hi - lo <= SEQUENTIAL_CUTOFF) {
+				for (int i = lo; i < hi; i++) {
+					solveBoardFromPoint(dictionary, board, 
+							new Point(i / board.getWidth(), i % board.getWidth()));
 				}
-			}
-			if (!threads.isEmpty()) {
-				for (int i = 0; i < threads.size() - 1; i++) {
-					threads.get(i).fork();
-				}
-				threads.get(threads.size() - 1).compute();
+			} else {
+				BoggleBotTask left = new BoggleBotTask(dictionary, board, lo, (hi + lo) / 2);
+				BoggleBotTask right = new BoggleBotTask(dictionary, board, (hi + lo) / 2, hi); 
+				left.fork();
+				right.compute();
 			}
 		}
 	}
